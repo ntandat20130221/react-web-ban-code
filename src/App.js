2c77a43a229f700c4bada7134a2d89de093829ac
tuyen <20130459@st.hcmuaf.edu.vn>
2023-05-16 11:34:04 +0700

    import css vào App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
+import SectionHero from "./components/section_hero"
+
+import './css/bootstrap.min.css'
+import './css/elegant-icons.css'
+import './css/font-awesome.min.css'
+import './css/nice-select.css'
+import './css/slicknav.min.css'
+import './css/style.css'
 
 function App() {
     return (
         <div>
             <Header/>
+            <SectionHero/>
             <Footer/>
         </div>
     )
