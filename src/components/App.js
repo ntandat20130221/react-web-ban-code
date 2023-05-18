40cb73f2d2c7f293b74fa1d094ddc6b1d205e92b
ntandat20130221 <20130221@st.hcmuaf.edu.vn>
2023-05-18 17:27:13 +0700

    Import thư viện react-router-dom và cấu hình router cho root và list-products

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import Header from "./components/header";
-import Footer from "./components/footer";
-import SectionHero from "./components/section_hero"
+import Header from "./header";
+import Footer from "./footer";
+import SectionHero from "./section_hero"
 
-import './css/bootstrap.min.css'
-import './css/elegant-icons.css'
-import './css/font-awesome.min.css'
-import './css/nice-select.css'
-import './css/slicknav.min.css'
-import './css/style.css'
+import '../css/bootstrap.min.css'
+import '../css/elegant-icons.css'
+import '../css/font-awesome.min.css'
+import '../css/nice-select.css'
+import '../css/slicknav.min.css'
+import '../css/style.css'
 
 function App() {
     return (
