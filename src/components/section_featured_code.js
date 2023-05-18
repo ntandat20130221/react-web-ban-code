8b1b0d9630f2555637fd06dab8b66138325b67b3
tuyen <20130459@st.hcmuaf.edu.vn>
2023-05-18 23:33:04 +0700

    thêm component featured_code.js (code nổi bật)

---
+++ src/components/section_featured_code.js
@@ -0,0 +1,24 @@
+
+
+function SectionFeaturedCode(){
+    return(
+        <section className="featured spad">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="section-title">
+                            <h2>CODE NỔI BẬT</h2>
+                        </div>
+                        <div className="featured__controls">
+                            <ul>
+                                <li className="active" data-filter="*">Android</li>
+                                <li data-filter=".oranges">iOS</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default SectionFeaturedCode;
