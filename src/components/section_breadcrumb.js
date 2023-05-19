da60f73d63ea6381a9ef06a19a8be72fb49569ac
tuyen <20130459@st.hcmuaf.edu.vn>
2023-05-19 08:44:14 +0700

    thêm component SectionBreadcrumb

---
+++ src/components/section_breadcrumb.js
@@ -0,0 +1,22 @@
+function SectionBreadcrumb() {
+    return (
+        <section className="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 text-center">
+                        <div className="breadcrumb__text">
+                            {/*<h2>Vegetable’s Package</h2>*/}
+                            <div className="breadcrumb__option">
+                                <a href="">Trang chủ</a>
+                                <a href="">Chi tiết sản phẩm</a>
+                                <span>App Tin tức</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SectionBreadcrumb;
