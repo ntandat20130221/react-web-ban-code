96eaf263388e0a5c214c94b8d20af84ed8d792f2
ntandat20130221 <20130221@st.hcmuaf.edu.vn>
2023-06-06 18:11:40 +0700

    Cập nhật sidebar

--- src/components/TopCodePage/ListProducts.js
+++ src/components/TopCodePage/ListProducts.js
@@ -6,24 +6,32 @@ import {useEffect, useRef, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {categorize, most, mostDownloaded, mostViewed, switchPage} from "../../redux/Action";
 import SectionBreadcrumb from "../Commons/SectionBreadcrumb";
+import {Link} from "react-router-dom";
 
 export function PopularCode() {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:9810/products?_sort=downloaded,viewed&_order=desc&_limit=10')
+            .then(res => res.json())
+            .then(json => setData(json.data))
+    }, [])
+
     return (
-        <div className="sidebar-item mt-5">
-            <h6 className="list-group-item font-weight-bolder">Code phổ biến</h6>
+        <div className="sidebar-item sidebar-popular mt-5">
+            <h6 className="list-group-item">Code phổ biến</h6>
             <div className="list-group">
-                {Array(9).fill(1).map((value, index) => (
-                    <a className="list-group-item d-flex align-items-center" key={index} href="#">
-                        <img className="mr-2" src={require("../../img/products/lp-1.jpg")} alt=""/>
-                        <span className="popular-title">Bundle 5 Android Studio games</span>
-                    </a>
+                {data.map((product, index) => (
+                    <Link to={`product/${product.id}`} className="list-group-item" key={index}>
+                        <img className="mr-2" src={product.img} alt=""/>
+                        <span className="popular-title">{product.name}</span>
+                    </Link>
                 ))}
             </div>
         </div>
     )
 }
 
-function SideBar() {
+function SideBar({type}) {
     const relTypes = useRef([])
     useEffect(() => {
         fetch(`http://localhost:9810/products`)
@@ -44,16 +52,24 @@ function SideBar() {
 
     const dispatch = useDispatch()
 
+    function handleClick(type) {
+        dispatch(categorize(type))
+        dispatch(switchPage(1))
+    }
+
     return (
         <div className="sidebar">
             <div className="sidebar-item">
-                <h6 className="list-group-item font-weight-bolder">Phân loại code</h6>
+                <h6 className="list-group-item">Phân loại code</h6>
                 <div className="list-group">
                     {relTypes.current.map((value, index) => (
-                        <div className="list-group-item d-flex justify-content-between align-items-center" key={index}
-                             onClick={() => dispatch(categorize(value.name))}>
-                            <span>{value.name}</span>
-                            <span className="badge badge-light font-weight-normal">{value.quantity}</span>
+                        <div className={`list-group-item ${value.name === type && 'item-active'}`} key={index}
+                             onClick={() => handleClick(value.name)}>
+                            <div>
+                                <span><i className={value.icon}></i></span>
+                                <span>{value.name}</span>
+                            </div>
+                            <span className="badge badge-light">{value.quantity}</span>
                         </div>
                     ))}
                 </div>
@@ -63,15 +79,15 @@ function SideBar() {
     )
 }
 
-function ProductItem({p}) {
+function ProductItem({p, navigate}) {
     return (
         <div className="product-item">
-            <a href={`/list-products/product/${p.id}`} className="product-item-img">
+            <Link to={`product/${p.id}`} className="product-item-img">
                 <img src={p.img} alt=""/>
-            </a>
+            </Link>
             <div className="product-item-title d-flex justify-content-center align-items-center text-center pt-2">
                 <div className="title-wrapper">
-                    <a href="">{p.name}</a>
+                    <Link to={`product/${p.id}`}>{p.name}</Link>
                 </div>
             </div>
             <div className="product-item-stats d-flex justify-content-between">
@@ -88,23 +104,23 @@ function ProductItem({p}) {
                 </div>
             </div>
             <div className="product-item-bottom d-flex justify-content-between align-items-center">
-                <a className="product-item-brand"><i className={p.type.icon}></i> {p.type.name}</a>
-                <a className="product-item-price">{p.price}đ</a>
+                <div className="product-item-brand" onClick={() => navigate(p.type.name)}><i className={p.type.icon}></i> {p.type.name}</div>
+                <Link to={`product/${p.id}`} className="product-item-price">{p.price}đ</Link>
             </div>
         </div>
     )
 }
 
-function ProductItemRow({p}) {
+function ProductItemRow({p, navigate}) {
     return (
         <div className="product-item-row mb-4">
             <div className="row no-gutters">
-                <a href={`/list-products/product/${p.id}`} className="product-item-img col-lg-4 pr-3">
+                <Link to={`product/${p.id}`} className="product-item-img col-lg-4 pr-3">
                     <img src={p.img} alt=""/>
-                </a>
+                </Link>
                 <div className="product-item-row-content col-lg-6">
-                    <a className="product-item-row-title">{p.name}</a>
-                    <a className="product-item-brand"><i className={p.type.icon}></i> {p.type.name}</a>
+                    <Link to={`product/${p.id}`} className="product-item-row-title">{p.name}</Link>
+                    <div className="product-item-brand"><i className={p.type.icon} onClick={() => navigate(p.type.name)}></i> {p.type.name}</div>
                     <div className="product-item-stars">
                         {Array(5).fill(1).map((value, index) => (<i className="fa fa-star" key={index}></i>))}
                     </div>
@@ -117,7 +133,7 @@ function ProductItemRow({p}) {
                 <div className="col-lg-2 d-flex flex-column justify-content-end align-items-end">
                     <div className="pr-3 pb-3">
                         <div className="product-item-row-price text-center">
-                            <a className="d-inline text-center">{p.price}đ</a>
+                            <Link to={`product/${p.id}`} className="d-inline text-center">{p.price}đ</Link>
                         </div>
                         <div className="d-flex justify-content-end">
                             <a className="product-item-action mr-1"><i className="fa fa-thumbs-up"></i></a>
@@ -131,15 +147,22 @@ function ProductItemRow({p}) {
 }
 
 function Products(props) {
+    const dispatch = useDispatch()
+
+    function navigate(type) {
+        dispatch(categorize(type))
+        dispatch(switchPage(1))
+    }
+
     return (
         <div className="row">
             {props.data.map((value, index) => {
                 return props.isGrid ?
                     (<div className="product-item-container col-lg-4 col-md-6 col-sm-6" key={index}>
-                        <ProductItem p={value}/>
+                        <ProductItem p={value} navigate={navigate}/>
                     </div>) :
                     (<div className="product-item-container col-12" key={index}>
-                        <ProductItemRow p={value}/>
+                        <ProductItemRow p={value} navigate={navigate}/>
                     </div>)
             })}
         </div>
@@ -204,13 +227,15 @@ function Filter({total, onLayout}) {
     )
 }
 
-function Pagination(props) {
+function Pagination({total}) {
     const sort = useSelector(state => state.listProductsReducer.sort)
     const currentPage = useSelector(state => state.listProductsReducer.page)
     const dispatch = useDispatch()
 
+    const numbers = [...Array(Math.ceil(total / 12) + 1).keys()].slice(1)
+
     function onSwitchPage(page) {
-        const pages = props.numbers.length
+        const pages = numbers.length
         const pageNow = page < 1 || page > pages ? currentPage : page
         dispatch(switchPage(pageNow))
         switch (sort) {
@@ -227,7 +252,7 @@ function Pagination(props) {
     return (
         <ul className="product-pagination float-right mt-3">
             <li onClick={() => onSwitchPage(currentPage - 1)}><i className="fa fa-chevron-left"></i></li>
-            {props.numbers.map((value, index) => (
+            {numbers.map((value, index) => (
                 <li className={value === currentPage && "active"} key={index} onClick={() => onSwitchPage(value)}>{value}</li>
             ))}
             <li onClick={() => onSwitchPage(currentPage + 1)}><i className="fa fa-chevron-right"></i></li>
@@ -238,11 +263,12 @@ function Pagination(props) {
 function ProductsContainer() {
     const currentPage = useSelector(state => state.listProductsReducer.page)
     const type = useSelector(state => state.listProductsReducer.type)
-
     const [products, setProducts] = useState([])
+    const [grid, setGrid] = useState(true)
     const relTotal = useRef(0)
+
     useEffect(() => {
-        let url = ''
+        let url
         if (type) {
             url = `http://localhost:9810/products?type.name=${type}&_page=${currentPage}&_limit=12`
         } else {
@@ -256,20 +282,17 @@ function ProductsContainer() {
             })
     }, [currentPage, type])
 
-    const numbers = [...Array(Math.ceil(relTotal.current / 12) + 1).keys()].slice(1)
-    const [grid, setGrid] = useState(true)
-
     return (
         <section className="product">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-3 col-md-5">
-                        <SideBar/>
+                        <SideBar type={type}/>
                     </div>
                     <div className="col-lg-9 col-md-7 pl-4">
                         <Filter total={relTotal.current} onLayout={(idGrid) => setGrid(idGrid)}/>
                         <Products isGrid={grid} data={products}/>
-                        <Pagination numbers={numbers}/>
+                        <Pagination total={relTotal.current}/>
                     </div>
                 </div>
             </div>
