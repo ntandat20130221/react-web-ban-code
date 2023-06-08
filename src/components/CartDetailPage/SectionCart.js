c1df72901ac8d7f22b3519d3cc2996b071abbccb
tuyen <20130459@st.hcmuaf.edu.vn>
2023-06-08 09:42:29 +0700

    nâng cấp giao diện của breadcrumb

--- src/components/CartDetailPage/SectionCart.js
+++ src/components/CartDetailPage/SectionCart.js
@@ -7,7 +7,7 @@ import Swal from 'sweetalert2';
 import {formatCurrency, getPercentDiscount} from "../../javascript/utils/Utils_Tuyen";
 import {getListDiscountCode} from "../../javascript/api"
 
-import {removeItemFromCart, updateDiscountPercent, updateDiscountCode} from "../../redux/redux_tuyen/Action_Tuyen";
+import {removeItemFromCart, updateDiscountCode, updateDiscountPercent} from "../../redux/redux_tuyen/Action_Tuyen";
 
 function SectionCart() {
 
@@ -63,7 +63,7 @@ function SectionCart() {
             </div>
         </section>
     )
-}
+} // => đây là component cha
 
 function ItemCart(data) {
 
@@ -106,7 +106,7 @@ function ItemCart(data) {
             </td>
         </tr>
     )
-}
+} // => đây là component con
 
 function FormInputDiscount() {
 
--- src/components/Commons/SectionBreadcrumb.js
+++ src/components/Commons/SectionBreadcrumb.js
@@ -1,22 +1,18 @@
 import breadcrumb_1 from '../../img/breadcrumb/breadcrumb_1.png'
 
 function SectionBreadcrumb() {
+
+    const styleBreadcrumb = {display: "flex", justifyContent: "center"}
+
     return (
-        <section className="breadcrumb-section set-bg" style={{ backgroundImage: `url(${breadcrumb_1})` }}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12 text-center">
-                        <div className="breadcrumb__text">
-                            <div className="breadcrumb__option">
-                                <a href="">Trang chủ</a>
-                                <a href="">Chi tiết sản phẩm</a>
-                                <span>App Tin tức</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <div className="container">
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb" style={styleBreadcrumb}>
+                    <li className="breadcrumb-item"><a href="#">Trang chủ</a></li>
+                    <li className="breadcrumb-item"><a href="#">Chi tiết giỏ hàng</a></li>
+                </ol>
+            </nav>
+        </div>
     )
 }
 
